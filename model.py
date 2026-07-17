0f9c9fb2b379ba082f0cd5e79683ee4a590eb07a
YourName <your-email@example.com>
2026-07-17 07:20:00 -0700

    second commit

--- model.py
+++ model.py
@@ -1,36 +1,67 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from pdf_reader import read_pdf
 import torch
 
-MODEL_PATH = "./model"
 
-print("loading ai model")
+MODEL_NAME = "Qwen/Qwen2-1.5B-Instruct"
 
-model = AutoModelForCausalLM.from_pretrained(MODEL_PATH)
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
 
-print(model)
+print("Loading Qwen model...")
+
+
+tokenizer = AutoTokenizer.from_pretrained(
+    MODEL_NAME
+)
+
+
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_NAME,
+    torch_dtype=torch.bfloat16,
+    device_map="auto"
+)
+
+
+print("Model loaded")
+
+
 
 def generate_response(text):
+
     try:
 
         prompt = f"""
-Summarize the following text:
+Summarize the following PDF text:
 
 {text}
+
+Provide a clear and short summary.
 """
 
 
         inputs = tokenizer(
             prompt,
             return_tensors="pt",
-            truncation=True
-        ).to(model.device)
+            truncation=True,
+            max_length=8000
+        )
+
+
+        inputs = {
+            k:v.to(model.device)
+            for k,v in inputs.items()
+        }
+
 
 
         output = model.generate(
+
             **inputs,
-            max_new_tokens=400
+
+            max_new_tokens=400,
+
+            temperature=0.7,
+
+            do_sample=True
+
         )
 
 
@@ -40,17 +71,22 @@ Summarize the following text:
         )
 
 
-        print(result)
-
         return result
 
 
+
     except Exception as e:
-        print("Error:", e)
+
+        print("AI ERROR:",e)
+
         return None
-    
-    
+
+
+
+
+
 def generate_normal_response(text):
+
 
     inputs = tokenizer(
         text,
@@ -58,20 +94,20 @@ def generate_normal_response(text):
         truncation=True
     )
 
-    print(model.config._name_or_path)
-    print(model.config.model_type)
-    print(model.config.architectures)
-    print(model.generation_config)
-    inputs = {k: v.to(model.device) for k, v in inputs.items()}
 
-    output = model.generate(
+    inputs = {
+        k:v.to(model.device)
+        for k,v in inputs.items()
+    }
+
+
+    output=model.generate(
         **inputs,
         max_new_tokens=250
     )
 
-    result = tokenizer.decode(
+
+    return tokenizer.decode(
         output[0],
         skip_special_tokens=True
     )
-
-    return result
